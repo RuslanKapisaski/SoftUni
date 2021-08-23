a249fc5afd11c184d7538ab285f1b409f595a826
Ruslan Kapisaski <ruslank1@abv.bg>
2021-08-23 15:55:53 +0300

    1st task

---
+++ Programming-Basics/Softuni 06.03.2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Softuni_06._03._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
