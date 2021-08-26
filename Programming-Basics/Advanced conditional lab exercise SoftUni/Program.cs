0497686d85d8519764a1b78b8de38568dea9e13c
Ruslan Kapisaski <ruslank1@abv.bg>
2021-08-26 22:39:08 +0300

    SoftUni

---
+++ Programming-Basics/Advanced conditional lab exercise SoftUni/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Advanced_conditional_lab_exercise_SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //INPUTS
+            string type = Console.ReadLine();
+            double rows = int.Parse(Console.ReadLine());
+            double columns = int.Parse(Console.ReadLine());
+
+            double price = 1;
+     
+            
+
+
+            if (type == "Premiere")
+            {
+                price= rows*columns*12;
+            }
+
+
+            if (type == "Normal")
+            {
+                price = rows * columns * 7.50;
+            }
+
+            if (type == "Discount")
+            {
+                price = rows * columns * 5.00;
+            }
+            Console.WriteLine($"{price:f2} leva");
+
+        }
+    }
+}
