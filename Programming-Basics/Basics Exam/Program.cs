bf4cef0830d8f36c013f7a2aa8d585c21b7a02d4
Ruslan Kapisaski <ruslank1@abv.bg>
2021-08-30 18:29:09 +0300

    Softuni

---
+++ Programming-Basics/Basics Exam/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EXAM_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int paper = int.Parse(Console.ReadLine());
+            int tissue = int.Parse(Console.ReadLine());
+            double glue = double.Parse(Console.ReadLine());
+            int discount = int.Parse(Console.ReadLine());
+
+            //logic
+            double paperPrice = 5.80;
+            double tissuePrice = 7.20;
+            double gluePrice = 1.20;
+
+            double totalPriceForPaper = paper * paperPrice;
+            double totalPriceForTissue = tissue * tissuePrice;
+            double totalPriceForGlue = glue * gluePrice;
+
+
+            double discount1 = (totalPriceForPaper + totalPriceForTissue + totalPriceForGlue) * discount/100;
+            double totalPrice = (totalPriceForPaper + totalPriceForTissue + totalPriceForGlue);
+            double finalTotalPrice = totalPrice - discount1;
+
+
+            Console.WriteLine($"{finalTotalPrice:f3}");
+        
+        }
+    }
+}
