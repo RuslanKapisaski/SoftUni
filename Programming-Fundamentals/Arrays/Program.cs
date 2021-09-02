2ccdefdd7435a5b76cbbb80bbbf28bbf19384e3b
Ruslan Kapisaski <ruslank1@abv.bg>
2021-09-02 20:03:30 +0300

    SoftUni

---
+++ Programming-Fundamentals/Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagoons = int.Parse(Console.ReadLine());
+
+            int countOfPeople = 0;
+            int sum = 0;
+            int peopleInTrain = 0;
+
+            int[] array = new int[wagoons];
+
+
+            for (int i = 0; i < wagoons; i++)
+            {
+                countOfPeople = int.Parse(Console.ReadLine());
+                array[i] = countOfPeople;
+                sum += countOfPeople;
+            }
+            peopleInTrain = countOfPeople;
+
+
+            for (int i = 0; i < wagoons; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.Write(sum);
+        }
+    }
+}
