8cf9f12934719537ed42921b20a2c9598ad8448e
Ruslan Kapisaski <ruslank1@abv.bg>
2021-09-03 15:02:53 +0300

    SoftUni

---
+++ Programming-Fundamentals/Comparison between 2 arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Comparison_between_2_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split(' ');
+            string[] secondArray = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                string currentArray = secondArray[i];
+
+
+                for (int l = 0; l < firstArray.Length; l++)
+                {
+                   
+
+                    if (currentArray == firstArray[l])
+                    {
+                        Console.Write(currentArray + " ");
+                    }
+                }
+            }
+
+        }
+    }
+}
