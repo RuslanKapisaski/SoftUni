47ef16c6754b8a872de88e4a626d874d787bf350
Ruslan Kapisaski <ruslank1@abv.bg>
2021-09-04 16:26:33 +0300

    SoftUni

---
+++ Programming-Fundamentals/Padawan equipment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Padawan_equipment
+{
+    class Program
+    {
+        static void Main(string[] rgs)
+        {
+            //inputs
+            double money = double.Parse(Console.ReadLine());
+            int countOfStudents = int.Parse(Console.ReadLine());
+            double priceOfLightsabers = double.Parse(Console.ReadLine());
+            double priceOfRobes = double.Parse(Console.ReadLine());
+            double priceOfBelts = double.Parse(Console.ReadLine());
+
+
+
+            //logic
+            double freeBelt = 0;  double totalMoneyNeeded = (countOfStudents *  priceOfRobes) + (priceOfBelts*( countOfStudents -  (countOfStudents/6)) +(Math.Ceiling(countOfStudents*1.1) * priceOfLightsabers));
+            
+           
+            if (money>=totalMoneyNeeded)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalMoneyNeeded:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"John will need {totalMoneyNeeded-money:F2}lv more.");
+            }
+
+
+        }
+    }
+}
